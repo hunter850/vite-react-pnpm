4b050958d68b52131799de38cf914c8f0d944514
hunter850 <hunter850711@gmail.com>
2025-04-30 01:37:33 +0800

    [Feature] Add useEffect to log environment variable on Greeting component mount

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 function Greeting(): React.JSX.Element {
+    useEffect(() => {
+        console.log("HELLO: ", process.env.VITE_HELLO);
+    }, []);
+
     return (
         <main className="mx-auto min-h-[calc(100vh-48px)] max-w-xl content-center p-8 text-center">
             <h1 className="text-5xl font-bold">Hello</h1>
