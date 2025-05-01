f3832a4845a8b106f9bf74868c0d2a9da59d5544
hunter850 <hunter850711@gmail.com>
2025-05-01 23:17:17 +0800

    [Feature] Enhance Greeting component to display personalized greeting based on URL search parameters

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -1,13 +1,17 @@
 import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
 function Greeting(): React.JSX.Element {
+    const [searchParams] = useSearchParams();
+    const name = searchParams.get("name");
+
     useEffect(() => {
         console.log("HELLO: ", import.meta.env.VITE_HELLO);
     }, []);
 
     return (
         <main className="mx-auto min-h-[calc(100vh-48px)] max-w-xl content-center p-8 text-center">
-            <h1 className="text-5xl font-bold">Hello</h1>
+            <h1 className="text-5xl font-bold">Hello {`${name ? name : "Anonymous"}`}</h1>
         </main>
     );
 }
